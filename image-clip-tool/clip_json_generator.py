93f7108b70df0f1af2ec683c8c8dcb10c45652e1
ssss <sdevdariani9@gmail.com>
2023-04-30 19:11:13 +0400

    Support for processing standalone images

--- image-clip-tool/clip_json_generator.py
+++ image-clip-tool/clip_json_generator.py
@@ -1,12 +1,7 @@
-#%pip install pillow torch openai-clip
-
 import os
-import zipfile
 import hashlib
 import json
 from PIL import Image
-import requests
-from io import BytesIO
 import clip
 import torch
 
@@ -40,57 +35,37 @@ def clip_json_generator(input_directory, output_directory):
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
+    image_data = []
+
     for root, dirs, files in os.walk(input_directory):
         for file in files:
-            if file.endswith('.zip'):
-                zip_file_path = os.path.join(root, file)
-                image_data = []
+            if file.lower().endswith(('.gif','.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp')):
+                image_file_path = os.path.join(root, file)
 
-                with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-                    for member in zip_ref.infolist():
-                        if member.filename.lower().endswith(('.gif','.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp')):
-                            # Extract image from the zip file
-                            with zip_ref.open(member) as img_file:
-                                img_data = img_file.read()
-                                img = Image.open(BytesIO(img_data))
-                                img.save(f'extracted_{os.path.basename(member.filename)}')
+                # Calculate file hash
+                file_hash = get_file_hash(image_file_path)
 
-                            # Calculate file hash
-                            file_hash = get_file_hash(f'extracted_{os.path.basename(member.filename)}')
+                # Generate CLIP vector
+                clip_vector = get_clip_vector(image_file_path)
 
-                            # Generate CLIP vector
-                            clip_vector = get_clip_vector(f'extracted_{os.path.basename(member.filename)}')
+                # Add the data to the list (if the CLIP vector is not None)
+                if clip_vector is not None:
+                    image_data.append({
+                        'filename': os.path.basename(image_file_path),
+                        'file_hash': file_hash,
+                        'clip_model': model_name,
+                        'clip_vector': clip_vector
+                    })
+                else:
+                    print(f"Skipping image {image_file_path} due to missing file.")
 
-                            # Add the data to the list (if the CLIP vector is not None)
-                            if clip_vector is not None:
-                                image_data.append({
-                                    'zipfile': os.path.basename(zip_file_path),  
-                                    'filename': os.path.basename(member.filename),  
-                                    'file_hash': file_hash,
-                                    'clip_model': model_name,
-                                    'clip_vector': clip_vector
-                                })
-                            else:
-                                print(f"Skipping image {member.filename} due to missing file.")
+    # Write the image_data list to a single JSON file
+    output_json_file = os.path.join(output_directory, "clip_vectors.json")
+    with open(output_json_file, 'w') as f:
+        json.dump(image_data, f, indent=4)
 
-                            # Clean up the extracted image
-                            os.remove(f'extracted_{os.path.basename(member.filename)}')
-
-                # Write the image_data list to a separate JSON file for each zip file
-                output_json_file = os.path.join(output_directory, f"{os.path.splitext(file)[0]}_clip_vectors.json")
-                with open(output_json_file, 'w') as f:
-                    json.dump(image_data, f, indent=4)
-
-#Change input_directory and output_directory paths with Correct paths
-#Existing input_directory path is from kcg-dataset/test-data. To run this code, clone kcg-dataset and change Working Directory.
 
 if __name__ == "__main__":
-    input_directory = './test_data'
-    output_directory = './output'
-    print("Starting image processing...")
+    input_directory = './input/Image-scraper-pixel-art-allimages'
+    output_directory = './output/clip_json'
     clip_json_generator(input_directory, output_directory)
-    print("Image processing finished!")
-
-
-# Also, you can run the script using the command:
-#python clip_json_generator.py /path/to/input/directory /path/to/output/directory
