60b58860402900760435047d608d05d082279024
ssss <sdevdariani9@gmail.com>
2023-04-30 23:07:24 +0400

    Update Generator

--- image-clip-tool/clip_json_generator.py
+++ image-clip-tool/clip_json_generator.py
@@ -1,13 +1,16 @@
 import os
+import zipfile
 import hashlib
 import json
 from PIL import Image
+import requests
+from io import BytesIO
 import clip
 import torch
 import argparse
+from tqdm import tqdm
 
 model_name = "ViT-L/14"
-
 
 def clip_json_generator(input_directory, output_directory):
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -36,35 +39,50 @@ def clip_json_generator(input_directory, output_directory):
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
-    image_data = []
+    zip_files = [file for root, _, files in os.walk(input_directory) for file in files if file.endswith('.zip')]
+    total_zip_files = len(zip_files)
+    print("Start process")
 
-    for root, dirs, files in os.walk(input_directory):
-        for file in files:
-            if file.lower().endswith(('.gif','.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp')):
-                image_file_path = os.path.join(root, file)
+    for file in tqdm(zip_files, desc="Processing zip files"):
+        zip_file_path = os.path.join(input_directory, file)
+        image_data = []
 
-                # Calculate file hash
-                file_hash = get_file_hash(image_file_path)
+        with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+            for member in zip_ref.infolist():
+                if member.filename.lower().endswith(('.gif','.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp')):
+                    # Extract image from the zip file
+                    with zip_ref.open(member) as img_file:
+                        img_data = img_file.read()
+                        img = Image.open(BytesIO(img_data))
+                        img.save(f'extracted_{os.path.basename(member.filename)}')
 
-                # Generate CLIP vector
-                clip_vector = get_clip_vector(image_file_path)
+                    # Calculate file hash
+                    file_hash = get_file_hash(f'extracted_{os.path.basename(member.filename)}')
 
-                # Add the data to the list (if the CLIP vector is not None)
-                if clip_vector is not None:
-                    image_data.append({
-                        'filename': os.path.basename(image_file_path),
-                        'file_hash': file_hash,
-                        'clip_model': model_name,
-                        'clip_vector': clip_vector
-                    })
-                else:
-                    print(f"Skipping image {image_file_path} due to missing file.")
+                    # Generate CLIP vector
+                    clip_vector = get_clip_vector(f'extracted_{os.path.basename(member.filename)}')
 
-    # Write the image_data list to a single JSON file
-    output_json_file = os.path.join(output_directory, "clip_vectors.json")
-    with open(output_json_file, 'w') as f:
-        json.dump(image_data, f, indent=4)
+                    # Add the data to the list (if the CLIP vector is not None)
+                    if clip_vector is not None:
+                        image_data.append({
+                            'zipfile': os.path.basename(zip_file_path),
+                            'filename': os.path.basename(member.filename),
+                            'file_hash': file_hash,
+                            'clip_model': model_name,
+                            'clip_vector': clip_vector
+                        })
+                    else:
+                        print(f"Skipping image {member.filename} due to missing file.")
 
+                    # Clean up the extracted image
+                    os.remove(f'extracted_{os.path.basename(member.filename)}')
+
+        # Write the image_data list to a separate JSON file for each zip file
+        output_json_file = os.path.join(output_directory, f"{os.path.splitext(file)[0]}_clip_vectors.json")
+        with open(output_json_file, 'w') as f:
+            json.dump(image_data, f, indent=4)
+
+    print("Finish process")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate CLIP vectors for images in a directory.")
@@ -73,3 +91,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     clip_json_generator(args.input_directory, args.output_directory)
+
+
+
+
